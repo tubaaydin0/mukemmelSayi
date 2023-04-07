f47187113504bbd14241983b868ce9475f80998a
tubaaydin0 <43508049+tubaaydin0@users.noreply.github.com>
2023-04-07 03:24:30 +0300

    Update Main.java

--- mukemmelSayi/src/Main.java
+++ mukemmelSayi/src/Main.java
@@ -3,22 +3,20 @@ public class Main {
     public static void main(String[] args) {
         /*Klavyeden girilen bir sayının mükemmel sayı olup/olmadığını bulan ve sayı mükemmel sayı ise ekrana “mükemmel sayıdır.” değilse “mükemmel sayı değildir.”
         ifadelerini ekrana yazan program*/
-        int sayi,carp=1,tempSayi=0;
+        int sayi,top=0;
         Scanner inp= new Scanner(System.in);
         System.out.println("Bir sayı giriniz: ");
         sayi=inp.nextInt();
         tempSayi=sayi;
-        int i=2; //En küçük pozitif bölen 2 olduğu için.
-        while(tempSayi!=1){
-            if(tempSayi%i==0){ 
-                tempSayi/=i;
-                carp*=i;
-                i=1;
+        int i=1;
+        while(i<sayi){
+            if(sayi%i==0){
+                top+=i;
             }
             i++;
         }
 
-        if(carp==sayi && sayi!=1){
+        if(top==sayi){
             System.out.println("Mükemmel sayıdır.");
         }
         else{
